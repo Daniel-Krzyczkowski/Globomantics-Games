38d626ab0581c12b770cf144df81fc53d76afb23
Daniel Krzyczkowski <daniel.krzyczkowski@hotmail.com>
2021-07-02 11:05:18 +0200

    Add project files with default action (#1)
    Setup CI/CD action flows

---
+++ src/web-game/Globomantics.CloudGame/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Globomantics.CloudGame.Models;
+using Globomantics.CloudGame.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace Globomantics.CloudGame.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IGameApiService _gameApiService;
+
+        public HomeController(ILogger<HomeController> logger,
+                              IGameApiService gameApiService)
+        {
+            _logger = logger;
+            _gameApiService = gameApiService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
