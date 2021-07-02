0d42fd1a11eec491308ba6aa90dfed7a533bfeb7
Daniel-Krzyczkowski <daniel.krzyczkowski@hotmail.com>
2021-07-02 10:41:38 +0200

    Initial commit

---
+++ src/web-game/Globomantics.CloudGame/Service/ApiService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace Globomantics.CloudGame.Service
+{
+    public interface IGameApiService
+    {
+        Task SaveUserScoreAsync(int score);
+    }
+
+    public class GameApiService : IGameApiService
+    {
+        IConfiguration _configuration;
+
+        public GameApiService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SaveUserScoreAsync(int score)
+        {
+            try
+            {
+                var connectString = _configuration["AzureSQL:ConnectionString"];
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(connectString);
+                var conn = new SqlConnection(builder.ConnectionString);
+                string insertString = @"INSERT INTO Score(Score) VALUES (@UserScore)";
+                SqlCommand cmd = new SqlCommand(insertString, conn);
+                cmd.Parameters.AddWithValue("@UserScore", score);
+                cmd.ExecuteNonQuery();
+                await conn.CloseAsync();
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("Connection string is null");
+            }
+        }
+    }
+}
